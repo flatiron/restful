6fc935ced1ca492c5e7aa300697bae1f70571957
Marak Squires <marak.squires@gmail.com>
2012-05-05 02:40:00 -0700

    [dist] First commit

---
+++ examples/server.js
@@ -0,0 +1,52 @@
+var http        = require('http'),
+    restful     = require('../lib/restful'),
+    resourceful = require('resourceful');
+
+//
+// Create a new Creature resource using the Resourceful library
+//
+var Creature = resourceful.define('creature', function () {
+  //
+  // Specify a storage engine
+  //
+  this.use('memory');
+  //
+  // Specify some properties with validation
+  //
+  this.string('name');
+  this.string('description');
+});
+
+//
+// Create a new Director routing map based on "Creature" resource
+//
+var router = restful.createRouter(Creature);
+
+//
+// Setup a very simple HTTP server to serve our routing map!
+//
+var server = http.createServer(function (req, res) {
+  req.chunks = [];
+  req.on('data', function (chunk) {
+    req.chunks.push(chunk.toString());
+  });
+  /*
+
+   Router will now dispatch all RESTFul urls for the Creature resource
+
+    POST    /Creature
+    GET     /Creature/1
+    PUT     /Creature/1
+    DELETE  /Creature/1
+
+  */
+  router.dispatch(req, res, function (err) {
+    if (err) {
+      res.writeHead(404);
+      res.end();
+    }
+    console.log('Served ' + req.url);
+  });
+});
+
+server.listen(8000);
---
+++ test/restful-api-test.js
@@ -0,0 +1,118 @@
+var vows = require('vows'),
+    assert = require('assert'),
+    APIeasy = require('api-easy'),
+    restful = require('../lib/restful');
+
+
+var formful = require('../lib/restful');
+
+var resourceful = require('resourceful');
+var http = require('http');
+
+
+//
+// 
+//
+var Creature = resourceful.define('creature', function () {
+  //
+  // Specify a storage engine
+  //
+  this.use('memory');
+
+  //
+  // Specify some properties with validation
+  //
+  this.string('name');
+  this.string('description');
+
+  //
+  // Specify timestamp properties
+  //
+  this.timestamps();
+});
+
+//
+// Now that the `Creature` prototype is defined
+// we can add custom logic to be available on all instances
+//
+Creature.prototype.feed = function (food) {
+  this.belly.push(food);
+};
+
+
+var router = restful.createRouter(Creature);
+
+
+var server = http.createServer(function (req, res) {
+  req.chunks = [];
+  req.on('data', function (chunk) {
+    req.chunks.push(chunk.toString());
+  });
+  router.dispatch(req, res, function (err) {
+    if (err) {
+      res.writeHead(404);
+      res.end();
+    }
+    console.log('Served ' + req.url);
+  });
+});
+
+server.listen(8000);
+
+var suite = APIeasy.describe('restful/restful-api-test');
+
+suite.use('localhost', 8000)
+  .setHeader('Content-Type', 'application/json')
+  .followRedirect(false)
+    .get('/Creature')
+      .expect(200)
+  .next()
+    .post('/Creature', {})
+      .expect(201)
+  .next()
+    .get('/Creature/1')
+      .expect(200)
+  .next()
+    .put('/Creature/1', { "name" : "Dragon" })
+      .expect(204)
+  .next()
+    .get('/Creature/1')
+      .expect(200)
+      .expect("should have correct name", function (err, res, body) {
+         var result = JSON.parse(body);
+         assert.equal(result.name, "Dragon");
+      })
+  .next()
+    .put('/Creature/1', { "name" : "Unicorn" })
+      .expect(204)
+  .next()
+    .get('/Creature/1')
+      .expect(200)
+      .expect("should have correct name", function (err, res, body) {
+         var result = JSON.parse(body);
+         assert.equal(result.name, "Unicorn");
+      })
+  .next()
+    .del('/Creature/1')
+      .expect(204)
+  .next()
+    .get('/Creature/1')
+      .expect(404)
+  .next()
+    .post('/Creature', {})
+      .expect(201)
+  .next()
+    .get('/Creature/2')
+      .expect(200)
+  .next()
+    .post('/Creature', { "name": "Dragon" })
+      .expect(201)
+  .next()
+    .get('/Creature/3')
+      .expect(200)
+      .expect("should have correct name", function (err, res, body) {
+         var result = JSON.parse(body);
+         assert.equal(result.name, "Dragon");
+      })
+
+.export(module);
